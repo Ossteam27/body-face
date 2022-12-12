8e2275e20e050657d32d877049dfa83a20bc8b74
Ossteam27 <120361686+Ossteam27@users.noreply.github.com>
2022-12-12 18:38:53 +0900

    Update body2.py

--- body2.py
+++ body2.py
@@ -1,8 +1,6 @@
 import cv2
-body = body_cascade.detectMultiScale(gray, 1.15, 5)
 
-for (x, y, w, h) in body:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 
-cv2.imshow('img', img)
-cv2.waitKey()
+img = cv2.imread('./image/standing_people.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
